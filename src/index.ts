4827c1e1ee2f934719068e774852ccc7e82190c0
August Deer <august@augustdeer.com>
2026-08-21 16:25:47 -0700

    feat: add Inter font

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-const monthSlots = document.getElementById("month-slots");
-const august = document.getElementById("august");
-if (monthSlots && august) {
-  const positionInfo = august.getBoundingClientRect();
-  monthSlots.style.width = positionInfo.width + "px";
-}
