22302e2fc8c56ee43ebf1c178be1538750c56d9e
August Deer <august@augustdeer.com>
2026-08-28 11:24:52 -0700

    fix: add url to configuration

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://augustdeer.com",
   fonts: [
     {
       provider: fontProviders.fontsource(),
