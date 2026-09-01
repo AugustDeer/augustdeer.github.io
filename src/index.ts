0f85197446749476e7dda0d10693b65c3f57d822
August Deer <august@augustdeer.com>
2026-09-01 16:58:02 -0700

    feat: aria-hidden labels

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,41 @@
-const ANIMATING_CLASS = "animating";
-const MONTHS = [
-  "September",
-  "October",
-  "November",
-  "December",
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-];
+if (!window.matchMedia(`(prefers-reduced-motion: reduce)`).matches) {
+  const ANIMATING_CLASS = "animating";
+  const MONTHS = [
+    "September",
+    "October",
+    "November",
+    "December",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+  ];
 
-const slotMachine = document.getElementById("slot-machine");
+  const slotMachine = document.getElementById("slot-machine");
 
-if (slotMachine) {
-  // Save and Remove content
-  slotMachine.innerHTML = `<span class="slot-machine-inner">${MONTHS.map((m) => `<span class="slot-machine-slot">${m}</span>`).join("")}</span>`;
+  if (slotMachine) {
+    // Save and Remove content
+    slotMachine.innerHTML = `
+      <span class="slot-machine-inner">
+        ${MONTHS.map(
+          (m, i) =>
+            `<span class="slot-machine-slot" ${
+              i === MONTHS.length - 1 ? "" : "aria-hidden=true"
+            }>${m}</span>`,
+        ).join("")}
+      </span>`;
 
-  // Start animation
-  slotMachine.classList.add(ANIMATING_CLASS);
+    // Start animation
+    slotMachine.classList.add(ANIMATING_CLASS);
 
-  // Cleanup on animation end
-  slotMachine.addEventListener("animationend", () => {
-    slotMachine.classList.remove(ANIMATING_CLASS);
-    slotMachine.outerHTML = "August";
-  });
+    // Cleanup on animation end
+    slotMachine.addEventListener("animationend", () => {
+      slotMachine.classList.remove(ANIMATING_CLASS);
+      slotMachine.outerHTML = "August";
+    });
+  }
 }
