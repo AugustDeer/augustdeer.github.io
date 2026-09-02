1b2bb46b48bfd6b463ffcd1fdc94c3d479691884
August Deer <august@augustdeer.com>
2026-09-01 22:02:06 -0700

    feat: use DocumentFragment for performance

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,36 @@
-if (!window.matchMedia(`(prefers-reduced-motion: reduce)`).matches) {
-  const ANIMATING_CLASS = "animating";
-  const MONTHS = [
-    "September",
-    "October",
-    "November",
-    "December",
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-  ];
+const ANIMATING_CLASS = "animating";
+const MONTHS = [
+  "September",
+  "October",
+  "November",
+  "December",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+];
 
+const fragment = new DocumentFragment();
+const childNode = document.createElement("span");
+childNode.className = "slot-machine-inner";
+fragment.append(childNode);
+
+MONTHS.forEach((m) => {
+  const span = document.createElement("span");
+  span.className = "slot-machine-slot";
+  span.innerHTML = m;
+  childNode.append(span);
+});
+
+if (!window.matchMedia(`(prefers-reduced-motion: reduce)`).matches) {
   const slotMachine = document.getElementById("slot-machine");
 
   if (slotMachine) {
-    // Save and Remove content
-    slotMachine.innerHTML = `
-      <span class="slot-machine-inner">
-        ${MONTHS.map(
-          (m, i) =>
-            `<span class="slot-machine-slot" ${
-              i === MONTHS.length - 1 ? "" : "aria-hidden=true"
-            }>${m}</span>`,
-        ).join("")}
-      </span>`;
+    slotMachine.replaceChildren(fragment);
 
     // Start animation
     slotMachine.classList.add(ANIMATING_CLASS);
@@ -35,7 +38,7 @@ if (!window.matchMedia(`(prefers-reduced-motion: reduce)`).matches) {
     // Cleanup on animation end
     slotMachine.addEventListener("animationend", () => {
       slotMachine.classList.remove(ANIMATING_CLASS);
-      slotMachine.outerHTML = "August";
+      slotMachine.innerHTML = "August";
     });
   }
 }
