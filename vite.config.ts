067dcf7664a4232ab87a4858f048b033df0e2020
August Deer <august@augustdeer.com>
2026-09-03 10:09:52 -0700

    fix: move tsc to ci step

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
 import type { UserConfig } from "vite";
 
-export default { resolve: { preserveSymlinks: true } } satisfies UserConfig;
+export default {
+  resolve: { preserveSymlinks: true },
+} satisfies UserConfig;
